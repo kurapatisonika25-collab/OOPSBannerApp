00382a4c6345960ed0edd55f32a2657595285652
kurapatisonika25-collab <kurapatisonika25@gmail.com>
2026-02-22 18:22:58 +0530

    UC3 printing oops banner using string

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,16 @@
-public class OOPSBannerApp{
-    public static void main(String args[]){
-        System.out.println("OOPS");
+public class OopsBannerApp {
+    public static void main(String[] args) {
+
+        String line1 = " OOO   OOO   PPPP   SSSS ";
+        String line2 = "O   O O   O  P   P  S     ";
+        String line3 = "O   O O   O  PPPP    SSS  ";
+        String line4 = "O   O O   O  P          S ";
+        String line5 = " OOO   OOO   P      SSSS  ";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
     }
 }
